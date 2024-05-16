f86ab2009387213433f68cdfb222b0ba1c0f1fca
chang <kennethchangla@gmail.com>
2024-05-16 16:33:56 +0800

    rename table

--- migrations/20240514141027-create_summaries_table.js
+++ migrations/20240514141027-create_summaries_table.js
@@ -3,7 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
     async up(queryInterface, Sequelize) {
-        await queryInterface.createTable('summaries', {
+        await queryInterface.createTable('Summaries', {
             id: {
                 allowNull: false,
                 autoIncrement: true,
@@ -36,6 +36,6 @@ module.exports = {
     },
 
     async down(queryInterface, Sequelize) {
-        await queryInterface.dropTable('summaries');
+        await queryInterface.dropTable('Summaries');
     }
 };
